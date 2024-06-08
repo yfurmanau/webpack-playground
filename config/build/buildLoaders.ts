bf83dec837df0715a6bd984f8f8ab6c1df069b1a
yfurmanau <yfurmanau@gmail.com>
2024-06-08 19:51:25 +0200

    add babel

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -55,5 +55,31 @@ export function buildLoaders(options: BuildOptions): ModuleOptions["rules"] {
     ],
   };
 
-  return [assetLoader, scssLoader, tsLoader, svgrLoader];
+  const babelLoader = {
+    test: /\.tsx?$/,
+    exclude: /node_modules/,
+    use: {
+      loader: "babel-loader",
+      options: {
+        presets: [
+          "@babel/preset-env",
+          "@babel/preset-typescript",
+          [
+            "@babel/preset-react",
+            {
+              runtime: isDev ? "automatic" : "classic",
+            },
+          ],
+        ],
+      },
+    },
+  };
+
+  return [
+    assetLoader,
+    scssLoader,
+    tsLoader,
+    // babelLoader,
+    svgrLoader,
+  ];
 }
